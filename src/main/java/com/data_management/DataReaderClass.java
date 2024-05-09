99277dbfa07db9595a24594ac08c7b9fef89a891
Filip Sulich <sulich.f@gmail.com>
2024-05-09 18:41:27 +0200

    data reader class

--- src/main/java/com/data_management/DataReaderClass.java
+++ src/main/java/com/data_management/DataReaderClass.java
@@ -1,2 +1,40 @@
-package com.data_management;public class DataReaderClass {
+package com.data_management;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DataReaderClass implements DataReader{
+    private String directoryPath;
+
+    public DataReaderClass(String directoryPath) {
+        this.directoryPath = directoryPath;
+    }
+
+    @Override
+    public void readData(DataStorage dataStorage) throws IOException {
+        try {
+            File file = new File(directoryPath);
+            File[] files = file.listFiles();
+            assert files != null;
+            for (File f : files) {
+                BufferedReader reader = new BufferedReader(new FileReader(f));
+                if (f.isFile() && f.getName().endsWith(".txt") && !f.getName().equals("Alert.txt")){
+                    while (reader.ready()) {
+                        String line = reader.readLine();
+                        String[] data = line.split(" ");
+                        int patientID = Integer.parseInt(data[2].substring(0,data[2].length() - 1));
+                        double measurementValue = Double.parseDouble(data[8].substring(0,data[8].length() - 1));
+                        String recordType = data[6].substring(0, data[6].length() - 1);
+                        long timeStamp = Long.parseLong(data[4].substring(0, data[4].length() - 1));
+                        dataStorage.addPatientData(patientID, measurementValue, recordType, timeStamp);
+                    }
+                }
+                reader.close();
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+    }
 }
