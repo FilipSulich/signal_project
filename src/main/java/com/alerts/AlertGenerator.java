14954511f84913246c33014da4fcf5a90855242b
Filip Sulich <sulich.f@gmail.com>
2024-05-09 22:53:04 +0200

    initial testing

--- src/main/java/com/alerts/AlertGenerator.java
+++ src/main/java/com/alerts/AlertGenerator.java
@@ -17,6 +17,27 @@ import java.util.logging.Logger;
 public class AlertGenerator {
     private DataStorage dataStorage;
 
+    public static void main(String[] args) {
+        DataStorage dataStorage = new DataStorage();
+        AlertGenerator alertGenerator = new AlertGenerator(dataStorage);
+        Patient patient = new Patient(123);
+        PatientRecord record1 = new PatientRecord(123, 80, "SystolicPressure", 1714376789050L);
+        PatientRecord record2 = new PatientRecord(123, 90, "DiastolicPressure", 1714376789051L);
+        PatientRecord record3 = new PatientRecord(123, 91, "Saturation", 1714376789052L);
+        PatientRecord record4 = new PatientRecord(123, 99, "Saturation", 1714376789053L);
+        PatientRecord record5 = new PatientRecord(123, 100, "ECG", 1714376789054L);
+        PatientRecord record6 = new PatientRecord(123, 50, "ECG", 1714376789055L);
+        PatientRecord record7 = new PatientRecord(123, 100, "ECG", 1714376789056L);
+        patient.addRecord(record1.getMeasurementValue(), record1.getRecordType(), record1.getTimestamp());
+        patient.addRecord(record2.getMeasurementValue(), record2.getRecordType(), record2.getTimestamp());
+        patient.addRecord(record3.getMeasurementValue(), record3.getRecordType(), record3.getTimestamp());
+        patient.addRecord(record4.getMeasurementValue(), record4.getRecordType(), record4.getTimestamp());
+        patient.addRecord(record5.getMeasurementValue(), record5.getRecordType(), record5.getTimestamp());
+        patient.addRecord(record6.getMeasurementValue(), record6.getRecordType(), record6.getTimestamp());
+        patient.addRecord(record7.getMeasurementValue(), record7.getRecordType(), record7.getTimestamp());
+        String alert = alertGenerator.evaluateDataString(patient);
+        System.out.println(alert);
+    }
     /**
      * Constructs an {@code AlertGenerator} with a specified {@code DataStorage}.
      * The {@code DataStorage} is used to retrieve patient data that this class
@@ -40,14 +61,19 @@ public class AlertGenerator {
      * @param patient the patient data to evaluate for alert conditions
      */
     public void evaluateData(Patient patient) {
-        List<PatientRecord> records = patient.getRecords(System.currentTimeMillis() - 1000 * 60 * 60, System.currentTimeMillis());
+        List<PatientRecord> records = patient.getRecords(1700000000000L, 1800000000000L);
         PatientRecord previousRecord = null;
         PatientRecord twoRecordsAgo = null;
 
+        System.out.println(records.size());
         for(PatientRecord record : records) {
+            if(previousRecord != null && previousRecord.getRecordType().equals("SystolicPressure") && previousRecord.getMeasurementValue() < 90 && record.getRecordType().equals("Saturation") && record.getMeasurementValue() < 92) {
+                triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Hypotensive hypoxemia detected", record.getTimestamp()));
+            }
             if(record.getRecordType().equals("SystolicPressure") || record.getRecordType().equals("DiastolicPressure")) {
               if((record.getRecordType().equals("SystolicPressure") && record.getMeasurementValue() > 180 || record.getMeasurementValue() < 90) || (record.getRecordType().equals("DiastolicPressure") && record.getMeasurementValue() > 120 || record.getMeasurementValue() < 60)){
-                    triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Critical pressure detected", record.getTimestamp()));
+                  System.out.println("Critical pressure detected");
+                  triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Critical pressure detected", record.getTimestamp()));
                 }
                 if(previousRecord != null && twoRecordsAgo != null &&
                         (record.getMeasurementValue() - previousRecord.getMeasurementValue() > 10 && previousRecord.getMeasurementValue() - twoRecordsAgo.getMeasurementValue() > 10) || (record.getMeasurementValue() - previousRecord.getMeasurementValue() < -10 && (previousRecord.getMeasurementValue() - twoRecordsAgo.getMeasurementValue()) < -10)) {
@@ -68,12 +94,60 @@ public class AlertGenerator {
                     triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Irregular heart beat detected", record.getTimestamp()));
                 }
             }
-            if(previousRecord != null && previousRecord.getRecordType().equals("SystolicPressure") && previousRecord.getMeasurementValue() < 90 && record.getRecordType().equals("Saturation") && record.getMeasurementValue() < 92) {
-                triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Hypotensive hypoxemia detected", record.getTimestamp()));
-            }
+
             twoRecordsAgo = previousRecord;
             previousRecord = record;
         }
+    }
+
+    public String evaluateDataString(Patient patient) {
+        List<PatientRecord> records = patient.getRecords(1700000000000L, 1800000000000L);
+        PatientRecord previousRecord = null;
+        PatientRecord twoRecordsAgo = null;
+        for(PatientRecord record : records) {
+            if(record.getRecordType().equals("SystolicPressure") && record.getMeasurementValue() < 90 && record.getRecordType().equals("Saturation") && record.getMeasurementValue() < 92) {
+                triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Hypotensive hypoxemia detected", record.getTimestamp()));
+                return "Hypotensive hypoxemia detected";
+            }
+            if(record.getRecordType().equals("SystolicPressure") || record.getRecordType().equals("DiastolicPressure")) {
+                if((record.getRecordType().equals("SystolicPressure") && record.getMeasurementValue() > 180 || record.getMeasurementValue() < 90) || (record.getRecordType().equals("DiastolicPressure") && record.getMeasurementValue() > 120 || record.getMeasurementValue() < 60)){
+                    triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Critical pressure detected", record.getTimestamp()));
+                    return "Critical pressure detected";
+                }
+                if(previousRecord != null && twoRecordsAgo != null && (previousRecord.getRecordType().equals("SystolicPressure") || previousRecord.getRecordType().equals("DiastolicPressure") && (twoRecordsAgo.getRecordType().equals("SystolicPressure") || twoRecordsAgo.getRecordType().equals("DiastolicPressure")))){
+                    if (Math.abs((record.getMeasurementValue() - previousRecord.getMeasurementValue())) > 10 && Math.abs(previousRecord.getMeasurementValue() - twoRecordsAgo.getMeasurementValue()) > 10) {
+                        triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Trend in blood pressure detected", record.getTimestamp()));
+                        return "Trend in blood pressure detected";
+                    }
+                }
+            } else if(record.getRecordType().equals("Saturation")) {
+                if(record.getMeasurementValue() < 92) {
+                    triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Low blood saturation detected", record.getTimestamp()));
+                    return "Low blood saturation detected";
+                }
+                if(previousRecord != null && previousRecord.getRecordType().equals("Saturation")){
+                    if(record.getTimestamp() - previousRecord.getTimestamp() <= 1000 * 60 * 10 && record.getMeasurementValue() - previousRecord.getMeasurementValue() < -5) {
+                        triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Rapid drop in blood saturation detected", record.getTimestamp()));
+                        return "Rapid drop in blood saturation detected";
+                    }
+                }
+            } else if(record.getRecordType().equals("ECG")){
+                if(record.getMeasurementValue() < 50 || record.getMeasurementValue() > 100) {
+                    triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Abnormal heart rate detected", record.getTimestamp()));
+                    return "Abnormal heart rate detected";
+                }
+                if(previousRecord != null && previousRecord.getRecordType().equals("ECG")){
+                    if(Math.abs(record.getMeasurementValue() - previousRecord.getMeasurementValue()) > 10) {
+                        triggerAlert(new Alert(String.valueOf(record.getPatientId()), "Irregular heart beat detected", record.getTimestamp()));
+                        return "Irregular heart beat detected";
+                    }
+                }
+            }
+
+            twoRecordsAgo = previousRecord;
+            previousRecord = record;
+        }
+        return null;
     }
     /**
      * Triggers an alert for the monitoring system. This method can be extended to
---
+++ src/test/java/data_management/AlertGeneratorTest.java
@@ -0,0 +1,82 @@
+package data_management;
+
+import com.alerts.AlertGenerator;
+import com.data_management.DataStorage;
+import com.data_management.Patient;
+import com.data_management.PatientRecord;
+import org.junit.*;
+import org.junit.jupiter.api.DisplayName;
+
+public class AlertGeneratorTest {
+    public DataStorage mockDataStorage;
+    public AlertGenerator mockAlertGenerator = new AlertGenerator(mockDataStorage);
+    public Patient mockPatient = new Patient(123);
+
+    @Test
+    @DisplayName("Critical pressure test")
+    public void evaluatePressure(){
+        PatientRecord mockRecord = new PatientRecord(123, 80, "SystolicPressure", 1714376789050L);
+        mockPatient.addRecord(mockRecord.getMeasurementValue(), mockRecord.getRecordType(), mockRecord.getTimestamp());
+        String alert = mockAlertGenerator.evaluateDataString(mockPatient);
+        Assert.assertEquals("Critical pressure detected", alert);
+    }
+    @Test
+    @DisplayName("Hypotensive hypoxemia test")
+    public void evaluateHypotensiveHypoxemia(){
+        PatientRecord mockRecord = new PatientRecord(123, 80, "SystolicPressure", 1714376789050L);
+        PatientRecord mockRecord2 = new PatientRecord(123, 90, "DiastolicPressure", 1714376789051L);
+        PatientRecord mockRecord3 = new PatientRecord(123, 91, "Saturation", 1714376789052L);
+        mockPatient.addRecord(mockRecord.getMeasurementValue(), mockRecord.getRecordType(), mockRecord.getTimestamp());
+        mockPatient.addRecord(mockRecord2.getMeasurementValue(), mockRecord2.getRecordType(), mockRecord2.getTimestamp());
+        mockPatient.addRecord(mockRecord3.getMeasurementValue(), mockRecord3.getRecordType(), mockRecord3.getTimestamp());
+        String alert = mockAlertGenerator.evaluateDataString(mockPatient);
+        Assert.assertEquals("Hypotensive hypoxemia detected", alert);
+    }
+    @Test
+    @DisplayName("Trend in blood pressure test")
+    public void evaluateTrend(){
+        PatientRecord mockRecord = new PatientRecord(123, 100, "SystolicPressure", 1714376789050L);
+        PatientRecord mockRecord2 = new PatientRecord(123, 111, "SysolicPressure", 1714376789051L);
+        mockPatient.addRecord(mockRecord.getMeasurementValue(), mockRecord.getRecordType(), mockRecord.getTimestamp());
+        mockPatient.addRecord(mockRecord2.getMeasurementValue(), mockRecord2.getRecordType(), mockRecord2.getTimestamp());
+        String alert = mockAlertGenerator.evaluateDataString(mockPatient);
+        Assert.assertEquals("Trend in blood pressure detected", alert);
+    }
+    @Test
+    @DisplayName("Low blood saturation test")
+    public void evaluateLowSaturation(){
+        PatientRecord mockRecord3 = new PatientRecord(123, 91, "Saturation", 1714376789052L);
+        mockPatient.addRecord(mockRecord3.getMeasurementValue(), mockRecord3.getRecordType(), mockRecord3.getTimestamp());
+        String alert = mockAlertGenerator.evaluateDataString(mockPatient);
+        Assert.assertEquals("Low blood saturation detected", alert);
+    }
+
+    @Test
+    @DisplayName("Rapid drop in blood saturation test")
+    public void evaluateRapidDrop(){
+        PatientRecord mockRecord3 = new PatientRecord(123, 99, "Saturation", 1714376789052L);
+        PatientRecord mockRecord4 = new PatientRecord(123, 93, "Saturation", 1714376789053L);
+        mockPatient.addRecord(mockRecord3.getMeasurementValue(), mockRecord3.getRecordType(), mockRecord3.getTimestamp());
+        mockPatient.addRecord(mockRecord4.getMeasurementValue(), mockRecord4.getRecordType(), mockRecord4.getTimestamp());
+        String alert = mockAlertGenerator.evaluateDataString(mockPatient);
+        Assert.assertEquals("Rapid drop in blood saturation detected", alert);
+    }
+    @Test
+    @DisplayName("Abnormal heart rate test")
+    public void evaluateHeartRate(){
+        PatientRecord mockRecord = new PatientRecord(123, 40, "ECG", 1714376789054L);
+        mockPatient.addRecord(mockRecord.getMeasurementValue(), mockRecord.getRecordType(), mockRecord.getTimestamp());
+        String alert = mockAlertGenerator.evaluateDataString(mockPatient);
+        Assert.assertEquals("Abnormal heart rate detected", alert);
+    }
+    @Test
+    @DisplayName("Irregular heart beat test")
+    public void evaluateIrregularHeartBeat(){
+        PatientRecord mockRecord = new PatientRecord(123, 40, "ECG", 1714376789054L);
+        PatientRecord mockRecord2 = new PatientRecord(123, 50, "ECG", 1714376789055L);
+        mockPatient.addRecord(mockRecord.getMeasurementValue(), mockRecord.getRecordType(), mockRecord.getTimestamp());
+        mockPatient.addRecord(mockRecord2.getMeasurementValue(), mockRecord2.getRecordType(), mockRecord2.getTimestamp());
+        String alert = mockAlertGenerator.evaluateDataString(mockPatient);
+        Assert.assertEquals("Irregular heart beat detected", alert);
+    }
+}
