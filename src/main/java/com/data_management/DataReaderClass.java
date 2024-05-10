2c01f63b4f176b3d370901dccbcd7042e2101782
Kanilok <kammotyl@gmail.com>
2024-05-10 13:03:51 +0200

    get records data test finished

--- src/main/java/com/data_management/DataReaderClass.java
+++ src/main/java/com/data_management/DataReaderClass.java
@@ -1,9 +1,6 @@
 package com.data_management;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class DataReaderClass implements DataReader{
     private String directoryPath;
@@ -17,20 +14,19 @@ public class DataReaderClass implements DataReader{
         try {
             File file = new File(directoryPath);
             File[] files = file.listFiles();
-            assert files != null;
             for (File f : files) {
-                BufferedReader reader = new BufferedReader(new FileReader(f));
-                if (f.isFile() && f.getName().endsWith(".txt") && !f.getName().equals("Alert.txt")){
-                    while (reader.ready()) {
-                        String line = reader.readLine();
-                        String[] data = line.split(" ");
-                        int patientID = Integer.parseInt(data[2].substring(0,data[2].length() - 1));
-                        double measurementValue = Double.parseDouble(data[8].substring(0,data[8].length() - 1));
-                        String recordType = data[6].substring(0, data[6].length() - 1);
-                        long timeStamp = Long.parseLong(data[4].substring(0, data[4].length() - 1));
-                        dataStorage.addPatientData(patientID, measurementValue, recordType, timeStamp);
+                    BufferedReader reader = new BufferedReader(new FileReader(f));
+                    if (f.isFile() && f.getName().endsWith(".txt") && !f.getName().equals("Alert.txt")){
+                        while (reader.ready()) {
+                            String line = reader.readLine();
+                            String[] data = line.split(" ");
+                            int patientID = Integer.parseInt(data[2].substring(0,data[2].length() - 1));
+                            double measurementValue = Double.parseDouble(data[8].substring(0,data[8].length() - 1));
+                            String recordType = data[6].substring(0, data[6].length() - 1);
+                            long timeStamp = Long.parseLong(data[4].substring(0, data[4].length() - 1));
+                            dataStorage.addPatientData(patientID, measurementValue, recordType, timeStamp);
+                        }
                     }
-                }
                 reader.close();
             }
         } catch (IOException e) {
--- src/test/java/data_management/DataReaderTest.java
+++ src/test/java/data_management/DataReaderTest.java
@@ -1,9 +1,63 @@
 package data_management;
 
+import com.data_management.DataReader;
 import com.data_management.DataReaderClass;
+import com.data_management.DataStorage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.data_management.PatientRecord;
+import org.junit.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class DataReaderTest {
-    public DataReaderClass mockDataReaderClass;
 
+
+    @Test
+    @DisplayName("Data read test")
+    public void readDataTest() throws IOException{
+
+        // create test directory with files
+        Path mockDirectory = Files.createTempDirectory("mockDirectory");
+        Path mockFile1 = Files.createFile(mockDirectory.resolve("mockFile1.txt"));
+        Path mockFile2 = Files.createFile(mockDirectory.resolve("mockFile2.txt"));
+
+        List<String> mockLines1 = new ArrayList<>() ;
+        mockLines1.add("Patient ID: 1, Timestamp: 1715269830535, Label: DiastolicPressure, Data: 70.0");
+        mockLines1.add("Patient ID: 2, Timestamp: 1715269830535, Label: SystolicPressure, Data: 80.0");
+        Files.write(mockFile1, mockLines1);
+
+        List<String> mockLines2 = new ArrayList<>() ;
+        mockLines1.add("Patient ID: 3, Timestamp: 1715269830535, Label: DiastolicPressure, Data: 90.0");
+        mockLines1.add("Patient ID: 4, Timestamp: 1715269830535, Label: SystolicPressure, Data: 100.0");
+        Files.write(mockFile2, mockLines2);
+
+        DataStorage dataStorage = new DataStorage();
+        DataReader dataReader = new DataReaderClass(mockDirectory.toString());
+        dataReader.readData(dataStorage);
+
+        List<PatientRecord> records1 = dataStorage.getRecords(1,1700000000000L, 1800000000000L);
+        assertEquals(70.0, records1.get(0).getMeasurementValue());
+        assertEquals("DiastolicPressure", records1.get(0).getRecordType());
+
+        List<PatientRecord> records2 = dataStorage.getRecords(2,1700000000000L, 1800000000000L);
+        assertEquals(80.0, records2.get(0).getMeasurementValue());
+        assertEquals("SystolicPressure", records1.get(0).getRecordType());
+
+        List<PatientRecord> records3 = dataStorage.getRecords(3,1700000000000L, 1800000000000L);
+        assertEquals(90.0, records3.get(0).getMeasurementValue());
+        assertEquals("DiastolicPressure", records1.get(0).getRecordType());
+
+        List<PatientRecord> records4 = dataStorage.getRecords(4,1700000000000L, 1800000000000L);
+        assertEquals(100.0, records4.get(0).getMeasurementValue());
+        assertEquals("SystolicPressure", records1.get(0).getRecordType());
+
+    }
 
 }
